857864b24a999c5b8c7a933628b8149b6bb31f82
Josef Filzmaier <j.filzmaier@student.tugraz.at>
2016-04-21 16:57:26 +0200

    Added new app "alwm", which stands for atmel lightweight mesh
    This includes a very basic program with RPL disabled and working makefile, which does nothing.

---
+++ apps/alwm/alwm.c
@@ -0,0 +1,12 @@
+
+#include "contiki.h"
+
+PROCESS(atmel_lightweight_mesh, "Atmel lwm");
+AUTOSTART_PROCESSES(&atmel_lightweight_mesh);
+
+PROCESS_THREAD(atmel_lightweight_mesh, ev, data) {
+
+    PROCESS_BEGIN();
+
+    PROCESS_END();
+}
