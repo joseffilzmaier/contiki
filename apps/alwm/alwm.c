74c0315335bf8437f2deff3fd587791d867fb8a3
Josef Filzmaier <j.filzmaier@student.tugraz.at>
2016-04-25 15:13:57 +0200

    Some modifications and testing to ALWM. Trying to disable Contiki network stack.

--- apps/alwm/alwm.c
+++ apps/alwm/alwm.c
@@ -1,5 +1,7 @@
 
 #include "contiki.h"
+#include "stdio.h"
+#include "alwm.h"
 
 PROCESS(atmel_lightweight_mesh, "Atmel lwm");
 AUTOSTART_PROCESSES(&atmel_lightweight_mesh);
@@ -7,6 +9,8 @@ AUTOSTART_PROCESSES(&atmel_lightweight_mesh);
 PROCESS_THREAD(atmel_lightweight_mesh, ev, data) {
 
     PROCESS_BEGIN();
+
+    //NwkFrameHeader_t header;
 
     PROCESS_END();
 }
